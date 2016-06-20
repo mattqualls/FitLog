e1e3feade7b1d934dbbb8b320b25bc99dc673139
Matt Qualls <mattqualls@comcast.net>
2016-06-20 10:52:20 -0500

    properties added for session class

--- FitLogMVC/FitLogMVC/Models/Session.cs
+++ FitLogMVC/FitLogMVC/Models/Session.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace FitLogMVC.Models
 {
     public class Session
     {
+        [Key]
+        public int SessionId { get; set; }
+        public int ApplUserId { get; set; }
+        public DateTime SessionDate { get; set; }
+        public int SessionWeight { get; set; }
+        public double MilesTraveled { get; set; }
+        public double MinutesWorked { get; set; }
+        public int CaloriesBurned { get; set; }
+
+        //[ForeignKey("ApplicationUserId")]
+        public AppUser AppUser { get; set; }
+
+        public ICollection<SessionType> SessionTypes { get; set; }
     }
 }
