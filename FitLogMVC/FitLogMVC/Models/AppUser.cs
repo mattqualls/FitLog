6e2fb4b9212bfd2318fa7d584d3dbb36064d1ca4
Matt Qualls <mattqualls@comcast.net>
2016-06-20 10:46:49 -0500

    add models for data structure w/ AppUser properties completed

---
+++ FitLogMVC/FitLogMVC/Models/AppUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FitLogMVC.Models
+{
+    public class AppUser
+    {
+        [Key]
+        public int AppUserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int InitialWeight { get; set; }
+        public int Age { get; set; }
+        public DateTime JoinDate { get; set; }
+
+        public ICollection<Session> Sessions { get; set; }
+    }
+}
---
+++ FitLogMVC/FitLogMVC/Models/FitLogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FitLogMVC.Models
+{
+    public class FitLogDbContext
+    {
+    }
+}
