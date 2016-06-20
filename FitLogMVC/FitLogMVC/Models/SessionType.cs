86e5a8005ef10494a6c8907687f6eec10578de00
Matt Qualls <mattqualls@comcast.net>
2016-06-20 10:54:26 -0500

    added sessiontype properties with appropriate FK relationship added

--- FitLogMVC/FitLogMVC/Models/SessionType.cs
+++ FitLogMVC/FitLogMVC/Models/SessionType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace FitLogMVC.Models
 {
     public class SessionType
     {
+        [Key]
+        public int SessionTypeId { get; set; }
+        public int SessionId { get; set; }
+        public int TreadmillId { get; set; }
+        public int BikeId { get; set; }
+        public int EllipticalId { get; set; }
+
+        public Session Session { get; set; }
     }
 }
